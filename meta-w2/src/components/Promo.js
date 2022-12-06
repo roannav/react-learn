7841438dcaa766ade9e23629314d9a5c74f5f76b
Roanna Victorio <roannav@users.noreply.github.com>
2022-12-05 16:15:31 -0800

    use a list of data

--- meta-w2/src/components/Promo.js
+++ meta-w2/src/components/Promo.js
@@ -1,14 +1,38 @@
+// Use a list of data in JSX
+
+const data = [
+  {
+    heading: "Don't miss this deal!",
+    callToAction: "Subscribe to my newsletter and get all the shop items at 50% off!"
+  },
+  {
+    heading: "99% off all t-shirts!",
+    callToAction: "Everything must go!"
+  }
+]
+
+function OnePromo(props) {
+  return (
+    <>
+      <h1>{props.heading}</h1>
+      <h2>{props.callToAction}</h2>
+    </>
+  );
+}
+
 function Promo() {
-    return (
-        <div className="promo-section">
-            <div>
-                <h1>Don't miss this deal!</h1>
-            </div>
-            <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
-            </div>
-        </div>
-    );
+  return (
+    <div className="promo-section">
+      <div className="one-promo">
+        {data.map((datum, index) => (
+          <OnePromo key={index}
+            heading={datum.heading}
+            callToAction={datum.callToAction}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Promo;
