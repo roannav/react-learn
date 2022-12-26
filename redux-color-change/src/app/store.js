ef1ad1af981a6023a3689cd2d41eb21fe9bf0550
Roanna Victorio <roannav@users.noreply.github.com>
2022-12-26 12:03:49 -0800

    add notes

--- redux-color-change/src/app/store.js
+++ redux-color-change/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-// in colorSlice, we did 'export default colorSlice.reducer'
+// in colorSlice, we did 'export default colorfulSlice.reducer'
 //  The name of the slice is 'color'
-import colorReducer from '../features/color/colorSlice';
+import colorDReducer from '../features/color/colorSlice';
 
 export const store = configureStore({
   reducer: {
-    color2: colorReducer,
+    color2: colorDReducer,
   },
 });
+// This is where the state variable (ie state.color2) is defined...
+// in the store's reducer object.
