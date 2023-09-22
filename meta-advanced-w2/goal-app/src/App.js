72be69c6b63327f2688432fab572697aca9431e4
Roanna Victorio <roannav@users.noreply.github.com>
2023-09-21 20:42:27 -0700

    simple goal app using useState hook

---
+++ meta-advanced-w2/goal-app/src/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./App.css";
+
+function GoalForm(props) {
+  const [formData, setFormData] = React.useState({ goal: "", by: "" });
+
+  function changeHandler(e) {
+    // ...formData is a shallow clone
+    // [e.target.name] The [] brackets let you set the object property name
+    // dynamically, so it'll be the value of e.target.name
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: "", by: "" });
+  }
+
+  return (
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal"
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By"
+          value={formData.by}
+          onChange={changeHandler}
+        />
+        <button>Submit Goal</button>
+      </form>
+    </>
+  );
+}
+
+function ListOfGoals(props) {
+  if (props.allGoals)
+    console.log("ListOfGoals allGoals length", props.allGoals.length);
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function App() {
+  const [allGoals, updateAllGoals] = React.useState([]);
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
+  }
+
+  return (
+    <div className="App-header">
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
+    </div>
+  );
+}
